6558e9d1c64b952ff7e92c3455377be6e9037d80
Damon Cook <damonacook@gmail.com>
2023-12-14 16:42:13 -0500

    🐛 FIX: Options page ERROR with function check

--- includes/acf-settings-page.php
+++ includes/acf-settings-page.php
@@ -5,105 +5,109 @@
  * @link https://www.advancedcustomfields.com/resources/options-page/
  */
 
-add_action(
-	'acf/init',
-	function () {
-		// Add the top-level page.
-		acf_add_options_page(
-			array(
-				'page_title' => 'Site Settings',
-				'menu_slug'  => 'site-settings',
-				'redirect'   => false,
-			)
-		);
+/**
+ * Check if ACF PRO is active and function exists.
+ */
+if ( function_exists( 'acf_add_options_page' ) ) {
+	add_action( 'acf/init', 'demo_acf_register_options_page' );
+}
 
-		// Add the sub-page.
-		acf_add_options_sub_page(
-			array(
-				'page_title'  => 'Contact Information',
-				'menu_slug'   => 'contact-information',
-				'parent_slug' => 'site-settings',
-			)
-		);
+function demo_acf_register_options_page() {
+	// Add the top-level page.
+	acf_add_options_page(
+		array(
+			'page_title' => 'Site Settings',
+			'menu_slug'  => 'site-settings',
+			'redirect'   => false,
+		)
+	);
 
-		// Add 'Contact Information' field group with Phone field.
-		acf_add_local_field_group(
-			array(
-				'key'      => 'group_6511a57f5680c',
-				'title'    => 'Contact Information',
-				'fields'   => array(
+	// Add the sub-page.
+	acf_add_options_sub_page(
+		array(
+			'page_title'  => 'Contact Information',
+			'menu_slug'   => 'contact-information',
+			'parent_slug' => 'site-settings',
+		)
+	);
+
+	// Add 'Contact Information' field group with Phone field.
+	acf_add_local_field_group(
+		array(
+			'key'      => 'group_6511a57f5680c',
+			'title'    => 'Contact Information',
+			'fields'   => array(
+				array(
+					'key'           => 'field_6511a57fcbe7e',
+					'label'         => 'Phone Number',
+					'name'          => 'demo_acf_phone_number',
+					'type'          => 'text',
+					'default_value' => '555 123-4567',
+				),
+			),
+			'location' => array(
+				array(
 					array(
-						'key'           => 'field_6511a57fcbe7e',
-						'label'         => 'Phone Number',
-						'name'          => 'demo_acf_phone_number',
-						'type'          => 'text',
-						'default_value' => '555 123-4567',
+						'param'    => 'options_page',
+						'operator' => '==',
+						'value'    => 'contact-information',
 					),
 				),
-				'location' => array(
-					array(
+			),
+		)
+	);
+
+	acf_add_local_field_group(
+		array(
+			'key'      => 'group_6511a5e8c1c15',
+			'title'    => 'Notification Bar',
+			'fields'   => array(
+				array(
+					'key'        => 'field_6511a5e897814',
+					'label'      => 'Notification Bar',
+					'name'       => 'demo_acf_notification_bar_group',
+					'aria-label' => '',
+					'type'       => 'group',
+					'layout'     => 'row',
+					'sub_fields' => array(
 						array(
-							'param'    => 'options_page',
-							'operator' => '==',
-							'value'    => 'contact-information',
+							'key'           => 'field_6511a5f597815',
+							'label'         => 'Notification On/Off',
+							'name'          => 'demo_acf_notification_onoff',
+							'type'          => 'true_false',
+							'message'       => 'Should the site-wide Notification Bar be showing?',
+							'default_value' => 1,
+							'ui_on_text'    => 'On',
+							'ui_off_text'   => 'Off',
+							'ui'            => 1,
 						),
-					),
-				),
-			)
-		);
-
-		acf_add_local_field_group(
-			array(
-				'key'      => 'group_6511a5e8c1c15',
-				'title'    => 'Notification Bar',
-				'fields'   => array(
-					array(
-						'key'        => 'field_6511a5e897814',
-						'label'      => 'Notification Bar',
-						'name'       => 'demo_acf_notification_bar_group',
-						'aria-label' => '',
-						'type'       => 'group',
-						'layout'     => 'row',
-						'sub_fields' => array(
-							array(
-								'key'           => 'field_6511a5f597815',
-								'label'         => 'Notification On/Off',
-								'name'          => 'demo_acf_notification_onoff',
-								'type'          => 'true_false',
-								'message'       => 'Should the site-wide Notification Bar be showing?',
-								'default_value' => 1,
-								'ui_on_text'    => 'On',
-								'ui_off_text'   => 'Off',
-								'ui'            => 1,
-							),
-							array(
-								'key'               => 'field_6511a5f597816',
-								'label'             => 'Notification Message',
-								'name'              => 'demo_acf_notification_message',
-								'type'              => 'textarea',
-								'conditional_logic' => array(
+						array(
+							'key'               => 'field_6511a5f597816',
+							'label'             => 'Notification Message',
+							'name'              => 'demo_acf_notification_message',
+							'type'              => 'textarea',
+							'conditional_logic' => array(
+								array(
 									array(
-										array(
-											'field'    => 'demo_acf_notification_onoff',
-											'operator' => '==',
-											'value'    => '1',
-										),
+										'field'    => 'demo_acf_notification_onoff',
+										'operator' => '==',
+										'value'    => '1',
 									),
 								),
 							),
 						),
 					),
 				),
-				'location' => array(
+			),
+			'location' => array(
+				array(
 					array(
-						array(
-							'param'    => 'options_page',
-							'operator' => '==',
-							'value'    => 'site-settings',
-						),
+						'param'    => 'options_page',
+						'operator' => '==',
+						'value'    => 'site-settings',
 					),
 				),
-			)
-		);
-	}
-);
+			),
+		)
+	);
+}
